df1776a27455757a92512fa6639d000e93a24bf3
KalininVitalii <105376147+KalininVitalii@users.noreply.github.com>
2023-02-14 18:31:58 +0200

    fixed

--- src/js/menu-mobile.js
+++ src/js/menu-mobile.js
@@ -4,17 +4,19 @@
     closeModalBtn: document.querySelector('[data-mobile-menu-close]'),
     modal: document.querySelector('[data-mobile-menu-modal]'),
   };
-
+  // -----------------------------------------
   const header = document.getElementById('header');
   const body = document.getElementById('page');
   let elem = document.querySelector('#menu-mobile');
   let mobLink = document.querySelectorAll('.mobile-menu__link');
+  // --------------------------------------------
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
+    // -----------------------------------------------------------------
 
     if (
       elem.classList.contains('is-hidden') ||
@@ -27,8 +29,10 @@
       body.classList.remove('no-scroll');
       header.classList.add('header--no-transparency');
     }
+    // ---------------------------------------------------------------------
   }
 
+  // -----------------------------------------------------
   [].forEach.call(mobLink, function (el) {
     el.addEventListener('click', function (e) {
       header.classList.add('header--no-transparency');
@@ -36,4 +40,5 @@
       body.classList.remove('no-scroll');
     });
   });
+  // ---------------------------------------------------------------
 })();
